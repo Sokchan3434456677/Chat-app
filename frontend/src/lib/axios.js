46996c7c567e6c306e935e80ea66ba02b9eba0d8
Ear Sokchan <sokchanear0@gmail.com>
2025-04-10 12:56:37 +0700

    Updated product folder with new changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://chat-app-lf6y.vercel.app",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001",
   withCredentials: true, // Ensure cookies are sent with requests
 });
