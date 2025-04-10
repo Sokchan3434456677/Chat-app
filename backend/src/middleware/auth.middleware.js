258420965369adf72353a297659472c52b64f7c5
Ear Sokchan <sokchanear0@gmail.com>
2025-04-10 13:15:39 +0700

    Updated product folder with new changes

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -29,3 +29,13 @@ export const protectRoute = async (req, res, next) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const generateToken = (userId) => {
+  try {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    return token;
+  } catch (error) {
+    console.error("Error generating token: ", error.message);
+    throw new Error("Token generation failed");
+  }
+};
