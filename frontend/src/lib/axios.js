f48080ee349ee737e5c2b8cebc87d57b5e2d3c74
Ear Sokchan <sokchanear0@gmail.com>
2025-04-10 12:59:18 +0700

    Updated product folder with new changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001",
+  baseURL: import.meta.env.VITE_API_URL || "https://chat-app-henna-two-66.vercel.app",
   withCredentials: true, // Ensure cookies are sent with requests
 });
